3d24d5e97aeee333c32653eca3a044b0f1883762
Daniel Attevelt <parallax@countermail.com>
2015-06-01 22:09:40 +0200

    Got WS2812B working, got addressing code working

---
+++ receiver/addr_test.ino
@@ -0,0 +1,60 @@
+const int UADDRTRIP = 14;
+const int MADDRTRIP = 15;
+const int LADDRTRIP = 16;
+const int TABLESIZE = 8;
+
+struct addrlookup {
+  int limit;
+  int address;
+};
+
+addrlookup addrmap[TABLESIZE]
+{
+  {10, 0},
+  {275, 4},
+  {391, 2},
+  {477, 6},
+  {541, 1},
+  {592, 5},
+  {634, 3},
+  {839, 7},
+};
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(UADDRTRIP, INPUT);
+  pinMode(MADDRTRIP, INPUT);
+  pinMode(LADDRTRIP, INPUT);
+}
+
+void loop() {
+  printval("Address: ", get_address());
+}
+
+unsigned int get_address()
+{
+    unsigned int address = 0;
+    address += (binread_triplet(UADDRTRIP) & 0x7) << 6;
+    address += (binread_triplet(MADDRTRIP) & 0x7) << 3;
+    address += (binread_triplet(LADDRTRIP) & 0x7);
+    
+    return address;
+}
+
+int binread_triplet(int pin)
+{
+    int analog_value = analogRead(pin);
+    for (int i=0;i<TABLESIZE;i++) {
+      if (analog_value < addrmap[i].limit) return addrmap[i].address;
+    }
+}
+
+
+void printval(char* label, int  value)
+{
+    char buf[4];
+    itoa(value, buf, 10);
+    Serial.write(label);
+    Serial.write(buf);
+    Serial.write("\n");
+}
--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -1,9 +1,21 @@
+#include "FastLED.h"
 #include <Servo.h>
 
-#include <Conceptinetics.h>
-//#include <SoftwareSerial.h>
+//#include <Conceptinetics.h>
+#include <SoftwareSerial.h>
 
 #define DMX_SLAVE_CHANNELS 5
+
+// How many leds in your strip?
+#define NUM_LEDS 5
+
+// For led chips like Neopixels, which have a data line, ground, and power, you just
+// need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
+// ground, and power), like the LPD8806 define both DATA_PIN and CLOCK_PIN
+#define DATA_PIN 11
+
+// Define the array of leds
+CRGB leds[NUM_LEDS];
 
 const int PWM_LED_R = 3;
 const int PWM_LED_G = 5;
@@ -14,36 +26,114 @@ const int STATUS_LED = 13;
 
 
 //SoftwareSerial mySerial(10, 11); // RX, TX
-DMX_Slave dmx_slave(DMX_SLAVE_CHANNELS);
+//DMX_Slave dmx_slave(DMX_SLAVE_CHANNELS);
 Servo pan_servo;
 Servo tilt_servo;
 
-void setup() {
+void setup()
+{
   // Setup DMX
-  dmx_slave.enable ();  
-  dmx_slave.setStartAddress (1);
-  
+  //  dmx_slave.enable ();
+  //  dmx_slave.setStartAddress (1);
+
   // Setup outputs
   pinMode(STATUS_LED, OUTPUT);
   pinMode(PWM_SRV_P, OUTPUT);
   pinMode(PWM_SRV_T, OUTPUT);
-  
+
   // Setup servo's
   pan_servo.attach(PWM_SRV_P);
   tilt_servo.attach(PWM_SRV_T);
 
+  // Setup leds
+  FastLED.addLeds<WS2812, DATA_PIN, RGB>(leds, NUM_LEDS);
+  Serial.begin(9600);
+
+  Serial.write("Start powercycle");
+  // Perform powercycle
+  power_cycle();
+
+
+
 }
 
-void loop() {
+void power_cycle()
+{
+  int i;
+  for (i = 0; i <= 255; i++) {
+    analogWrite(PWM_LED_R, i);
+    analogWrite(PWM_LED_G, i);
+    analogWrite(PWM_LED_B, i);
+    change_servo_pos(pan_servo, i);
+    change_servo_pos(tilt_servo, i);
+    delay(10);
+  }
+  for (i = 255; i >= 0; i--) {
+    analogWrite(PWM_LED_R, i);
+    analogWrite(PWM_LED_G, i);
+    analogWrite(PWM_LED_B, i);
+    change_servo_pos(pan_servo, i);
+    change_servo_pos(tilt_servo, i);
+    delay(10);
+  }
+}
+
+void loop()
+{
+  handle_leds();
+  power_cycle();
+  /*
   int pan, tilt;
-  
+
   analogWrite(PWM_LED_R, dmx_slave.getChannelValue(1));
   analogWrite(PWM_LED_G, dmx_slave.getChannelValue(2));
   analogWrite(PWM_LED_B, dmx_slave.getChannelValue(3));
-  
+
   pan = map(dmx_slave.getChannelValue(4), 0, 255, 10, 179);     // scale it to use it with the servo (value between 0 and 180)
   pan_servo.write(pan);
-  
+
   tilt = map(dmx_slave.getChannelValue(5), 0, 255, 10, 179);
   tilt_servo.write(tilt);
+  */
+}
+
+void handle_leds()
+{
+  for (int i = 0; i < 2; i++) {
+    // Turn the LED on, then pause
+    leds[0].r = 255;
+    leds[0].g = 0;
+    leds[0].b = 0;
+
+    leds[1].r = 255;
+    leds[1].g = 255;
+    leds[1].b = 0;
+
+    leds[2].r = 0;
+    leds[2].g = 255;
+    leds[2].b = 0;
+
+    /*
+    leds[2] = CRGB::Red;
+    leds[3] = CRGB::Red;
+    leds[4] = CRGB::Red;*/
+    FastLED.show();
+    delay(500);
+    // Now turn the LED off, then pause
+    leds[0] = CRGB::Black;
+    leds[1] = CRGB::Black;
+    leds[2] = CRGB::Black;
+    /*
+    leds[2] = CRGB::Black;
+    leds[3] = CRGB::Black;
+    leds[4] = CRGB::Black;*/
+    FastLED.show();
+    delay(500);
+  }
+}
+
+void change_servo_pos(Servo servo, int position)
+{
+  int mapped_pos = map(position, 0, 255, 1, 179);
+  servo.write(mapped_pos);
 }
