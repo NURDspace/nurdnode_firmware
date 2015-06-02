130554c75bbfa6e01d31202d79645c501f1f8660
Daniel Attevelt <parallax@countermail.com>
2015-06-02 21:15:19 +0200

    Created some logic that will pulse the address on the LED

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -1,87 +1,117 @@
 #include "FastLED.h"
 #include <Servo.h>
+#include <Conceptinetics.h>
 
-//#include <Conceptinetics.h>
-#include <SoftwareSerial.h>
+#define DMX_SLAVE_CHANNELS 6
+#define PU_DELAY 250
 
-#define DMX_SLAVE_CHANNELS 5
+// Micro program ids
+#define SHOW_ADDRESS 1
 
-// How many leds in your strip?
-#define NUM_LEDS 5
 
-// For led chips like Neopixels, which have a data line, ground, and power, you just
-// need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
-// ground, and power), like the LPD8806 define both DATA_PIN and CLOCK_PIN
-#define DATA_PIN 11
-
-// Define the array of leds
-CRGB leds[NUM_LEDS];
-
+// Pin constants
 const int PWM_LED_R = 3;
 const int PWM_LED_G = 5;
 const int PWM_LED_B = 6;
 const int PWM_SRV_P = 9;
 const int PWM_SRV_T = 10;
-const int STATUS_LED = 13;
+const int UADDRTRIP = 14;
+const int MADDRTRIP = 15;
+const int LADDRTRIP = 16;
 
+// Addressing init section
+const int TABLESIZE = 8;
 
-//SoftwareSerial mySerial(10, 11); // RX, TX
-//DMX_Slave dmx_slave(DMX_SLAVE_CHANNELS);
+struct addrlookup {
+  int limit;
+  int address;
+};
+
+addrlookup addrmap[TABLESIZE]
+{
+  {10, 0},
+  {275, 4},
+  {391, 2},
+  {477, 6},
+  {541, 1},
+  {592, 5},
+  {634, 3},
+  {839, 7},
+};
+
+int address;
+
+// Init dmx_slave
+DMX_Slave dmx_slave(DMX_SLAVE_CHANNELS);
+
+// Init PT servo's
 Servo pan_servo;
 Servo tilt_servo;
 
+// Home positions (these may change due to calibration)
+int pan_home = 128;
+int tilt_home = 128;
+
 void setup()
 {
-  // Setup DMX
-  //  dmx_slave.enable ();
-  //  dmx_slave.setStartAddress (1);
+  // Wait a bit to allow all the caps in the array to charge up, and not have the servo's and LED
+  // pull extra current. Poor PSU.
+  delay(PU_DELAY);
 
-  // Setup outputs
-  pinMode(STATUS_LED, OUTPUT);
+  // Setup led channel outputs
+  pinMode(PWM_LED_R, OUTPUT);
+  pinMode(PWM_LED_G, OUTPUT);
+  pinMode(PWM_LED_B, OUTPUT);
+
+  // Setup servo outputs
   pinMode(PWM_SRV_P, OUTPUT);
   pinMode(PWM_SRV_T, OUTPUT);
+
+  // Setup addressing input pins
+  pinMode(UADDRTRIP, INPUT);
+  pinMode(MADDRTRIP, INPUT);
+  pinMode(LADDRTRIP, INPUT);
 
   // Setup servo's
   pan_servo.attach(PWM_SRV_P);
   tilt_servo.attach(PWM_SRV_T);
 
-  // Setup leds
-  FastLED.addLeds<WS2812, DATA_PIN, RGB>(leds, NUM_LEDS);
-  Serial.begin(9600);
+  // Get the DMX adress of our device
+  address = get_address();
+  
+  // Setup DMX
+  dmx_slave.enable();
+  dmx_slave.setStartAddress(address);
 
-  Serial.write("Start powercycle");
-  // Perform powercycle
-  power_cycle();
-
-
-
+  // Set servos to their home position
+  servos_go_home();
+  turn_off_LED();
+  
+  show_address();
 }
 
-void power_cycle()
+/*
+void printval(char* label, int  value)
 {
-  int i;
-  for (i = 0; i <= 255; i++) {
-    analogWrite(PWM_LED_R, i);
-    analogWrite(PWM_LED_G, i);
-    analogWrite(PWM_LED_B, i);
-    change_servo_pos(pan_servo, i);
-    change_servo_pos(tilt_servo, i);
-    delay(10);
-  }
-  for (i = 255; i >= 0; i--) {
-    analogWrite(PWM_LED_R, i);
-    analogWrite(PWM_LED_G, i);
-    analogWrite(PWM_LED_B, i);
-    change_servo_pos(pan_servo, i);
-    change_servo_pos(tilt_servo, i);
-    delay(10);
-  }
+    char buf[4];
+    itoa(value, buf, 10);
+    Serial.write(label);
+    Serial.write(buf);
+    Serial.write("\n");
 }
+*/
+void turn_off_LED()
+{
+  analogWrite(PWM_LED_R, 0);
+  analogWrite(PWM_LED_G, 0);
+  analogWrite(PWM_LED_B, 0);
+}
+
 
 void loop()
 {
-  handle_leds();
-  power_cycle();
+  show_address();
+  
   /*
   int pan, tilt;
 
@@ -94,46 +124,70 @@ void loop()
 
   tilt = map(dmx_slave.getChannelValue(5), 0, 255, 10, 179);
   tilt_servo.write(tilt);
-  */
+
+  switch (dmx_slave.getChannelValue(6))
+  {
+    case SHOW_ADDRESS:
+      show_address();
+      break;
+  }*/
 }
 
-void handle_leds()
+/**
+* Show the DMX address of the node
+*/
+void show_address()
 {
-  for (int i = 0; i < 2; i++) {
-    // Turn the LED on, then pause
-    leds[0].r = 255;
-    leds[0].g = 0;
-    leds[0].b = 0;
 
-    leds[1].r = 255;
-    leds[1].g = 255;
-    leds[1].b = 0;
-
-    leds[2].r = 0;
-    leds[2].g = 255;
-    leds[2].b = 0;
-
-    /*
-    leds[2] = CRGB::Red;
-    leds[3] = CRGB::Red;
-    leds[4] = CRGB::Red;*/
-    FastLED.show();
+  analogWrite(PWM_LED_B, 255);  
+  delay(1000);
+  turn_off_LED();
+  delay(500);
+  int bitmask = 256;
+  for (int i = 0; i < 9; i++) {
+    if ((address & (bitmask >> i)) == (bitmask >> i)) {
+      analogWrite(PWM_LED_G, 255);
+    } else {
+      analogWrite(PWM_LED_R, 255);
+    }
     delay(500);
-    // Now turn the LED off, then pause
-    leds[0] = CRGB::Black;
-    leds[1] = CRGB::Black;
-    leds[2] = CRGB::Black;
-    /*
-    leds[2] = CRGB::Black;
-    leds[3] = CRGB::Black;
-    leds[4] = CRGB::Black;*/
-    FastLED.show();
+    turn_off_LED();
     delay(500);
   }
+  turn_off_LED();
+  analogWrite(PWM_LED_B, 255);  
+  delay(1000);
+}
+
+/**
+* OHM sweet OHM
+*/
+void servos_go_home()
+{
+  change_servo_pos(pan_servo, pan_home);
+  change_servo_pos(tilt_servo, tilt_home);
 }
 
 void change_servo_pos(Servo servo, int position)
 {
   int mapped_pos = map(position, 0, 255, 1, 179);
   servo.write(mapped_pos);
+}
+
+unsigned int get_address()
+{
+  unsigned int address = 0;
+  address += (binread_triplet(UADDRTRIP) & 0x7) << 6;
+  address += (binread_triplet(MADDRTRIP) & 0x7) << 3;
+  address += (binread_triplet(LADDRTRIP) & 0x7);
+
+  return address;
+}
+
+int binread_triplet(int pin)
+{
+  int analog_value = analogRead(pin);
+  for (int i = 0; i < TABLESIZE; i++) {
+    if (analog_value < addrmap[i].limit) return addrmap[i].address;
+  }
 }
